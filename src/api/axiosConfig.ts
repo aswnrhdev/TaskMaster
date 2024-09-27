1fe29f63930f5b69a5ac1d3d8129eb143ee7e151
Aswin Ramesh <>
2024-09-27 20:36:13 +0530

    made some changes

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,9 +1,35 @@
-import axios from "axios";
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL as string;
 
-console.log('BASE_URL:',BASE_URL);
+console.log('BASE_URL:', BASE_URL);
 
-const Api = axios.create({baseURL:BASE_URL,withCredentials:true});
+const Api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true
+});
 
-export default Api; 
+interface UserInfo {
+  token: string;
+}
+
+Api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const userInfoString = localStorage.getItem('userInfo');
+    if (userInfoString) {
+      const userInfo: UserInfo = JSON.parse(userInfoString);
+      const token = userInfo.token;
+
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error);
+  }
+);
+
+export default Api;
